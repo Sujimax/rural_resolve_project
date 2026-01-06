e741c01ace72af8e6ce848fc0a907ec5cdb74253
Sujimax <sujithra1050@gmail.com>
2026-01-06 14:48:15 +0530

    Cloudinary Configured

--- backend/main.py
+++ backend/main.py
@@ -2,12 +2,23 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from database import engine, Base
-
+import os
+import cloudinary
 from router.user_complaint import user_complaint
 from router.admin import admin_router
 from router.auth import auth_router
 
+
 app = FastAPI(title="Complaint Management API")
+
+
+cloudinary.config(
+    cloud_name = os.getenv("CLOUDINARY_CLOUD_NAME"),
+    api_key = os.getenv("CLOUDINARY_API_KEY"),
+    api_secret = os.getenv("CLOUDINARY_API_SECRET"),
+    secure=True
+)
+
 
 # Create tables
 Base.metadata.create_all(bind=engine)
