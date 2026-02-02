7bcd59ebbf9743b523c119719f3053184f65b1d4
Sujimax <sujithra1050@gmail.com>
2026-02-02 22:56:50 +0530

    modified back into admin page

--- backend/router/admin.py
+++ backend/router/admin.py
@@ -7,40 +7,25 @@ from schemas.status_update import StatusUpdate
 
 admin_router = APIRouter(prefix="/admin", tags=["admin"])
 
-
-def send_email_background(email, complaint_id, status):
-    send_status_email(email, complaint_id, status)
-
-
+# Update complaint status
 @admin_router.put("/complaints/{complaint_id}")
 def update_complaint_status(
     complaint_id: int,
     data: StatusUpdate,
     db: Session = Depends(get_db),
-    admin=Depends(get_current_admin)
+    admin = Depends(get_current_admin)
 ):
     complaint = db.query(Complaint).filter(Complaint.id == complaint_id).first()
-
     if not complaint:
         raise HTTPException(status_code=404, detail="Complaint not found")
 
-    # update status
     complaint.status = data.status
     db.commit()
-
-    # send email automatically
-    if complaint.email:
-        Thread(
-            target=send_email_background,
-            args=(complaint.email, complaint.id, complaint.status)
-        ).start()
+    db.refresh(complaint)
 
     return {
-        "success": True,
-        "message": "Status updated successfully"
+        "message": "Status updated successfully",
     }
-    
-    
 
 @admin_router.delete("/complaints/{complaint_id}")
 def admin_delete_complaint(
