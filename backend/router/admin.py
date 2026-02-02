3f169705adf588f67fa65b6eb5d26b19bfe9aa78
Sujimax <sujithra1050@gmail.com>
2026-02-02 22:28:52 +0530

    error part updated in admin page

--- backend/router/admin.py
+++ backend/router/admin.py
@@ -4,17 +4,16 @@ from sqlalchemy.orm import Session
 from dependancy import get_db, get_current_admin
 from models.complaint_model import Complaint
 from schemas.status_update import StatusUpdate
-from utils.email import send_status_email
 
 admin_router = APIRouter(prefix="/admin", tags=["admin"])
 
-
+# Update complaint status
 @admin_router.put("/complaints/{complaint_id}")
 def update_complaint_status(
     complaint_id: int,
     data: StatusUpdate,
     db: Session = Depends(get_db),
-    admin=Depends(get_current_admin)
+    admin = Depends(get_current_admin)
 ):
     complaint = db.query(Complaint).filter(Complaint.id == complaint_id).first()
     if not complaint:
@@ -24,39 +23,20 @@ def update_complaint_status(
     db.commit()
     db.refresh(complaint)
 
-    # send email only if valid email exists
-    if complaint.email and "@" in complaint.email:
-        try:
-            print(f"📧 Sending email to: {complaint.email}")
-            send_status_email(
-                to_email=complaint.email,
-                complaint_id=complaint.id,
-                status=complaint.status
-            )
-        except Exception as e:
-            print(f"❌ Could not send email: {e}")
-
     return {
         "message": "Status updated successfully",
-        "complaint_id": complaint.id,
-        "status": complaint.status
     }
-
-
-# DELETE COMPLAINT (ADMIN)
 
 @admin_router.delete("/complaints/{complaint_id}")
 def admin_delete_complaint(
     complaint_id: int,
     db: Session = Depends(get_db),
-    admin=Depends(get_current_admin)
+    admin = Depends(get_current_admin)
 ):
     complaint = db.query(Complaint).filter(Complaint.id == complaint_id).first()
-
     if not complaint:
         raise HTTPException(status_code=404, detail="Complaint not found")
 
     db.delete(complaint)
     db.commit()
-
     return {"message": "Complaint deleted successfully"}
