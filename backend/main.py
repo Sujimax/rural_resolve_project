d6e4408be56cbfd3713374beead9fed82110511b
Sujimax <sujithra1050@gmail.com>
2026-03-25 18:58:59 +0530

    fix: main & database page

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from database import engine, Base
 import os
 import cloudinary
+
 from router.user_complaint import user_complaint
 from router.admin import admin_router   
 from router.auth import auth_router
@@ -10,9 +11,9 @@ from router.auth import auth_router
 app = FastAPI(title="Complaint Management API")
 
 cloudinary.config(
-    cloud_name = os.getenv("CLOUDINARY_CLOUD_NAME"),
-    api_key = os.getenv("CLOUDINARY_API_KEY"),
-    api_secret = os.getenv("CLOUDINARY_API_SECRET"),
+    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.getenv("CLOUDINARY_API_KEY"),
+    api_secret=os.getenv("CLOUDINARY_API_SECRET"),
     secure=True
 )
 
@@ -25,6 +26,11 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+# ✅ Test route (avoid 404)
+@app.get("/")
+def home():
+    return {"message": "API is working 🚀"}
 
 app.include_router(auth_router)
 app.include_router(user_complaint)
